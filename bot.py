4561e8554959d164dffb30ddea5298877f5d4fcd
sakshyam shrestha <sakshyam@woonholiday.localdomain>
2020-10-16 11:19:01 +0545

    yo help working but is ass

--- bot.py
+++ bot.py
@@ -25,24 +25,33 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    res = [
+    sixty_nine = [
          'Nice!!', 'VSDK', 'Hot funeral selfies', 'I slay it, Queen'
     ]
-    howgay = ['sala 100% gay ho yaar ', '100%', 'risheb ta lastai gay ho']
+    binayak = ['popat', 'xito udh', 'seliya', 'shellu']
+    how_gay = ['sala 100% gay ho yaar ', '100%', 'risheb ta lastai gay ho', 'binayak jati nai gay ho', 'infinitely gay']
 
-    budi = ['Preeti ho preeti', 'Binayak vsdk ho', 'binayak ra risheb honeymoon ma gaisakyo', 'Rekha thapa ho fix']
+    budi = ['Preeti ho preeti', 'Binayak vsdk ho', 'binayak ra risheb honeymoon ma gaisakyo', 'Rekha thapa ho fix', 'mula single nai marxa', 'college ko sab kt haru']
 
-    if message.content == 'howgay risheb':
-        response = random.choice(howgay)
+    commands = ['howgay risheb', 'risheb ko budi ko ho', '69', 'roast binayak']
+    if message.content == commands[0]:
+        response = random.choice(how_gay)
         await message.channel.send(response)
 
-    if message.content == 'risheb ko budi ko ho?':
+    if message.content == commands[1]:
         response = random.choice(budi)
         await message.channel.send(response)
 
-    if message.content == '69':
-        response = random.choice(res)
+    if message.content == commands[2]:
+        response = random.choice(sixty_nine)
         await message.channel.send(response)
 
-client.run(TOKEN)
+    if message.content == commands[3]:
+        response = random.choice(binayak)
+        await message.channel.send(response)
 
+    if message.content == 'yo help':
+        for i in enumerate(commands):
+            await message.channel.send(i)
+
+client.run(TOKEN)
